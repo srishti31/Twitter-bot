17a2bb58ab7f4153353ae28766d5aaf428fc1a11
srishti gupta-bng <Srishti_Gupta-BNG@mckinsey.com>
2016-12-30 23:56:57 +0530

    search tweets on nodejs

--- bot.js
+++ bot.js
@@ -4,8 +4,20 @@ var config = require('./config.js');
 
 var Twitter = new twit(config);
 
-Twitter.post('statuses/update', { status: "I'm posting a tweet, using my Twitter bot! -Developed by me :)" }, function(err, data, response) {
-  if(err) {
-    console.log("There was a problem tweeting the message.", err);
+var retweet = function(){
+  var params = {
+    q: '#Nodejs',
+    result_type: 'recent',
+    lang: 'en'
   }
-});
+
+  Twitter.get('search/tweets', params, function(err, data, response){
+    if(!err) {
+      console.log('Tweet' , data);
+    } else {
+          console.log('Something went wrong while SEARCHING...', data);
+    }
+  });
+}
+
+retweet();
