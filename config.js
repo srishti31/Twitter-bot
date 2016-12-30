b7795fb008bfc2147d3b194ca7668f8bdc73763c
srishti gupta-bng <Srishti_Gupta-BNG@mckinsey.com>
2016-12-30 23:47:09 +0530

    minor changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   consumer_key: 'P2T4qZMs6N2OnMgQ8DSsx6Y8r',
   consumer_secret: 'uHAaBlYQKhqUPntR5N1rbdP5oQSslJP1ZurZVrk6y4tit4LMPG',
-  access_token: '	2850401935-sjOimWrOTQwz7dpUs6TNcy2NoXdm5C47aonqcB5',
+  access_token: '2850401935-sjOimWrOTQwz7dpUs6TNcy2NoXdm5C47aonqcB5',
   access_token_secret: 'riYm7mJGV1AVTNz2UfuQlhsaZTY0lQ2tAkV82tIY5Q8ih'
 }
