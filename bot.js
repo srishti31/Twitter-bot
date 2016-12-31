6483f2b00d9b51b3dd7e009c3faeff1756207d27
srishti gupta-bng <Srishti_Gupta-BNG@mckinsey.com>
2016-12-31 22:09:40 +0530

    search tweets on nodejs

--- bot.js
+++ bot.js
@@ -1,15 +1,14 @@
 var twit = require('twit');
 var config = require('./config.js');
 
-
 var Twitter = new twit(config);
-
-var retweet = function(){
-  var params = {
-    q: '#reactjs, #Reactjs',
-    result_type: 'recent',
-    lang: 'en'
-  }
+var stream = Twitter.stream('user');
+const params = {
+  q: '#reactjs, #Reactjs',
+  result_type: 'recent',
+  lang: 'en'
+}
+const retweet = () => {
 
   Twitter.get('search/tweets', params)
   .catch(err => {
@@ -28,8 +27,47 @@ var retweet = function(){
   });
 }
 
-retweet();
-setInterval(retweet, 3000);
+// retweet();
+function ranDom (arr, n){
+  const index = Math.floor(Math.random()*n);
+  return arr[index];
+}
+const favouriteTweet = () => {
+  Twitter.get('search/tweets', params)
+  .catch(err => {
+    console.log('Something went wrong while SEARCHING...');
+  })
+  .then((result) => {
+    const tweet = result.data.statuses;
+    // console.log(tweet);
+    const randomTweet = ranDom(tweet, tweet.length);
+    if (typeof randomTweet != 'undefined') {
+      Twitter.post('favorites/create', { id: randomTweet.id_str })
+      .catch(err => {
+        console.log('cant find favorite');
+      })
+      .then(() => {
+        console.log('Favorited!');
+      })
+    }
+  });
+}
+favouriteTweet();
+
+stream.on('follow')
+.then((event) => {
+  console.log('Follow event is running');
+  const name = event.source.name;
+  const screenName = event.source.screen_name;
+  Twitter.post('direct_messages/new', { text: '@' + screenName + ' Thankyou for the follow up'})
+  .catch(err => {
+    console.log('cant DM');
+  })
+  .then(() => {
+    console.log('Gratitude shown successfully!');
+  })
+})
+// setInterval(retweet, 3000);
 // Twitter.post('statuses/update', { status: 'hello world- Twitter bot!' }, function(err, data, response) {
 //   console.log(data)
 // })
