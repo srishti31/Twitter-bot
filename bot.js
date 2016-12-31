5e7e76fc70d9518ef7d91fc8dc172a803853749b
srishti gupta-bng <Srishti_Gupta-BNG@mckinsey.com>
2016-12-31 15:54:41 +0530

    search tweets on nodejs

--- bot.js
+++ bot.js
@@ -6,18 +6,30 @@ var Twitter = new twit(config);
 
 var retweet = function(){
   var params = {
-    q: '#Nodejs',
+    q: '#reactjs, #Reactjs',
     result_type: 'recent',
     lang: 'en'
   }
 
-  Twitter.get('search/tweets', params, function(err, data, response){
-    if(!err) {
-      console.log('Tweet' , data);
-    } else {
-          console.log('Something went wrong while SEARCHING...', data);
-    }
+  Twitter.get('search/tweets', params)
+  .catch(err => {
+    console.log('Something went wrong while SEARCHING...');
+  })
+  .then((result) => {
+    const retweetId = result.data.statuses[1].id_str;
+    Twitter.post('statuses/retweet/:id', { id: retweetId })
+    .catch(err => {
+      console.log('Something went wrong with RETWEETING!!');
+    })
+    .then( response => {
+        console.log('RETWEETED!');
+    });
+    console.log(result.data);
   });
 }
 
 retweet();
+setInterval(retweet, 3000);
+// Twitter.post('statuses/update', { status: 'hello world- Twitter bot!' }, function(err, data, response) {
+//   console.log(data)
+// })
